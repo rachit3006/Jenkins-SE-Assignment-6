2ecd5cb66525c2c6a378962d94dbd9b903453f3b
Rachit Agrawal <34854802+rachit3006@users.noreply.github.com>
2022-11-25 01:50:51 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -11,19 +11,3 @@ def div(x,y):
     if(y==0):
         return "Division by zero not possible !"
     return x/y    
-
-if __name__=="__main__":
-    x = float(input("Enter first number : "))
-    y = float(input("Enter second number : "))
-    op = input("Which operation do you want to perform ? (+ or - or * or /) ")
-    
-    if (op=='+'):
-        print(add(x,y))
-    elif (op=='-'):
-        print(sub(x,y))
-    elif (op=='/'):
-        print(div(x,y))
-    elif (op=='*'):
-        print(mul(x,y))
-    else:
-        print("Invalid input !")
